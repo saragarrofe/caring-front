0bb3d72d4e69f57993b682beffa260ad1aa6e747
Sara Garrofé <s.garrofe@gmail.com>
2025-09-20 14:08:12 +0200

    Adds ProfileHeader component inside Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,9 +2,21 @@ import ProfileHeader from "@components/Profile/ProfileHeader";
 
 
 export default function Profile() {
+
+    const user = { 
+        name: "Sara Garrofé",
+        email: "example@example",
+        avatarUrl: "https://placehold.co/80x80",
+        joinedDate: "Septiembre 2025"
+    }
+
+    const handleEdit = () => {
+        // TO DO: navegar a /profile/edit 
+        alert('Navegar a /profile/edit')
+    }
     return (
-        <div>
-            <ProfileHeader />
-        </div>
+        <main className="container py-3">
+            <ProfileHeader name={user.name} email={user.email} avatarUrl={user.avatarUrl} joinedDate={user.joinedDate} onEdit={handleEdit} />
+        </main>
     )
 }
