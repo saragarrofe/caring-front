d2f7e3a822b7a5a9c039d46de02fbb399259cba4
Sara Garrofé <s.garrofe@gmail.com>
2025-09-27 15:29:40 +0200

    Create PlantCard to simplify mapping plants o MyPlants page

---
+++ src/components/Plants/PlantCard.tsx
@@ -0,0 +1,41 @@
+
+import { Link } from "react-router-dom";
+
+type Plant = {
+    id: number;
+    name: string;
+    species: string;
+    wateringFrequency: number;
+    lastWatered: string;
+    imageUrl?: string;
+}
+
+type PlantCardProps = {
+    plant: Plant;
+};
+
+export default function PlantCard({ plant }: PlantCardProps) {
+    return (
+        <div className="card h-100">
+            {plant.imageUrl && (
+            <div key={plant.id} className="row">
+                <img 
+                src={plant.imageUrl} 
+                className="card-img-top" 
+                alt={`Imagen de ${plant.name} de la especie ${plant.species}`} 
+                loading="lazy"  
+                />
+            </div>
+            )}
+            <div className="card-body">
+                <p className="card-title">{plant.name}</p>
+                <p className="card-subtitle mb-2 text-muted">Especie: {plant.species}</p>
+                <p className="card-text">Riego cada {plant.wateringFrequency} días</p>
+                <p className="card-text">Último riego: {plant.lastWatered}</p>
+                <Link to={`my-plants/${plant.id}`} className="btn btn-primary">
+                    Ver detalles
+                </Link>
+            </div>            
+        </div>
+    )
+}
--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,42 +1,21 @@
 
-import { Link } from 'react-router-dom';
 import { mockPlants } from '../mocks/plants';
+import PlantCard from '../components/Plants/PlantCard';
 
 export default function MyPlants() {
   return (
     <main className="container py-4">
-        <h1>Mis Plantas</h1>
+      <h1>Mis Plantas</h1>
         <div className="row">
-            {mockPlants.map(plant => (
-                <div key={plant.id} className="col-md-4 mb-4">
-                    <div className="card h-100">
-                        {plant.imageUrl && <img src={plant.imageUrl} className="card-img-top" alt={plant.name} />}
-                        <div className="card-body">
-                            <h5 className="card-title">{plant.name}</h5>
-                            <p className="card-text">Especie: {plant.species}</p>
-                            <p className="card-text">Frecuencia de riego: cada {plant.wateringFrequency} días</p>
-                            <p className="card-text">Último riego: {new Date(plant.lastWatered).toLocaleDateString()}</p>
-                            <Link to={`/my-plants/${plant.id}`} className="btn btn-primary">Ver Detalles</Link>
-                        </div>
-                    </div>
-                </div>
-            ))}
+          {mockPlants.map((plant) => (
+            <div key={plant.id} className='col-6 col-md-4 mb-4'>
+              <PlantCard plant={plant} />
+            </div>
+          ))}
+
         </div>
 
-        <ul className="list-group">
-        {mockPlants.map((p) => (
-          <li key={p.id} className="list-group-item d-flex justify-content-between align-items-center">
-            <span>
-              <strong>{p.name}</strong>
-              {p.species ? ` — ${p.species}` : ''}
-            </span>
-            {/* Link navega sin recargar la página */}
-            <Link to={`/my-plants/${p.id}`} className="btn btn-sm btn-primary">
-              Ver detalle
-            </Link>
-          </li>
-        ))}
-      </ul>
+        
 
     </main>
 
