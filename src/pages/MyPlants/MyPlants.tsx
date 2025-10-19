7f45b0c998a901cf038d6cbe23037fe213f88a5f
Sara Garrofe <s.garrofe@gmail.com>
2025-10-19 23:28:17 +0200

    feat(MyPlants): add MyPlants component with styling and layout

---
+++ src/pages/MyPlants/MyPlants.tsx
@@ -0,0 +1,29 @@
+import './MyPlants.css';
+
+import { mockPlants } from '../../mocks/plants';
+import {PlantCard} from '../../components';
+import BackButton from '@components/BackButton/BackButton';
+
+export default function MyPlants() {
+  return <>
+    <header className="page-header">
+        <div className="page-toolbar">
+          <BackButton fallback="/my-plants" />
+        </div>
+        <h1 className="page-title">My Plants</h1>
+      </header>
+    <div className="container py-3">
+      <div className="row row-cols-2 row-cols-md-3 g-3"> 
+        {mockPlants.map(p => (
+          <div key={p.id} className="col">
+            <PlantCard plant={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  
+  
+  
+  </>
+
+};
