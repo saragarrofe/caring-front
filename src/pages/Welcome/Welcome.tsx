3898d4e4664c52cd1e6f5feac6e36f21adfb7c52
Sara Garrofé <s.garrofe@gmail.com>
2025-11-07 22:10:43 +0100

    feat: add Welcome component with styling and navigation links

---
+++ src/pages/Welcome/Welcome.tsx
@@ -0,0 +1,42 @@
+import './Welcome.css';
+import { Link } from 'react-router-dom';
+
+export default function Welcome() {
+  return (
+    <main className="welcome">
+      <header className="welcome-header">
+        <h1 className="welcome-title">Welcome to Caring</h1>
+        <p className="welcome-sub">Reminders, tips and a happy jungle at home</p>
+      </header>
+      <div className="welcome-cta">
+        <Link to="/login" className="btn btn-primary">
+          LOG IN
+        </Link>
+        <Link to="/register" className="btn btn-outline">
+          SIGN UP
+        </Link>
+        <div className="welcome-legal">
+          By signing in you confirm our&nbsp;
+          {/* TO DO: Añadir handleTerms */}
+          <a href="#" onClick={(e) => e.preventDefault()}>
+            Terms
+          </a>
+          &nbsp;&amp;&nbsp;
+          {/* TO DO: Añadir handlePrivacyPolicy */}
+          <a href="#" onClick={(e) => e.preventDefault()}>
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+      {/* TO DO: Añadir handleSkipRegistration */}
+      <button
+        className="welcome-skip"
+        onClick={() => {
+          window.location.href = '/discover';
+        }}
+      >
+        Skip registration
+      </button>
+    </main>
+  );
+}
